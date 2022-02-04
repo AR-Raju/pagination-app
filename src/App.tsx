91d888b35fc9bcfeb7b350a754d7fda286932f40
AR-Raju <asikurrahmanraju@gmail.com>
2022-02-04 12:42:42 +0600

    router test case added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Details from "./components/Details";
 
+const NoMatch = () => <div>No match</div>;
+
 function App() {
   return (
     <div data-testid="app">
@@ -10,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/details" component={Details} />
+          <Route path="*" component={NoMatch} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,9 +1,46 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import { MemoryRouter, Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
-test("renders app test", () => {
-  render(<App />);
-  const linkElement = screen.getByTestId("app");
-  expect(linkElement).toBeInTheDocument();
+jest.mock("../components/Home.tsx", () => {
+  const MockName = () => <div>HomePageMock</div>;
+  return MockName;
+});
+
+describe("should test react router", () => {
+  test("should render Home page component", () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("HomePageMock")).toBeInTheDocument();
+  });
+
+  test("should render Details page component", () => {
+    const history = createMemoryHistory({ initialEntries: ["/details"] });
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+
+    expect(history.location.pathname).toBe("/details");
+  });
+
+  test("landing on a bad page", () => {
+    const history = createMemoryHistory({
+      initialEntries: ["/some/bad/routes"],
+    });
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+
+    expect(history.location.pathname).toBe("/some/bad/routes");
+  });
 });
